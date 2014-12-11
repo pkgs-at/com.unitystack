96fe15b7ab94d26def02e6bf62187ee61c7537b6
Sotaro SUZUKI <sotaro.suzuki@architector.jp>
2014-12-11 17:27:15 +0900

    Diagnostics #DEBUG付与

--- source/at.pkgs.logging/Properties/AssemblyInfo.cs
+++ source/at.pkgs.logging/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // アセンブリに関する一般情報は以下の属性セットをとおして制御されます。
 // アセンブリに関連付けられている情報を変更するには、
 // これらの属性値を変更してください。
-[assembly: AssemblyTitle("at.pkgs.logging")]
+[assembly: AssemblyTitle("At.Pkgs.Logging")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Architector Inc.")]
-[assembly: AssemblyProduct("at.pkgs.logging")]
-[assembly: AssemblyCopyright("Copyright (c) 2009-2013, Architector Inc., Japan All rights reserved.")]
+[assembly: AssemblyProduct("At.Pkgs.Logging")]
+[assembly: AssemblyCopyright("Copyright (c) 2009-2014, Architector Inc., Japan All rights reserved.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // すべての値を指定するか、下のように '*' を使ってビルドおよびリビジョン番号を 
 // 既定値にすることができます:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.1.0")]
-[assembly: AssemblyFileVersion("1.0.1.0")]
+[assembly: AssemblyVersion("1.0.2.0")]
+[assembly: AssemblyFileVersion("1.0.2.0")]
--- source/at.pkgs.logging/Sink/DiagnosticsDebugAppender.cs
+++ source/at.pkgs.logging/Sink/DiagnosticsDebugAppender.cs
@@ -15,6 +15,8 @@
  * limitations under the License.
  */
 
+#define DEBUG
+
 using System;
 using System.Diagnostics;
 
